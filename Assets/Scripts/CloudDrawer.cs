df0f687876198a93fcc997158c8239ac307d7261
Alex Wang <wangtianyoualex@gmail.com>
2024-05-03 02:38:39 -0700

    BuildTest_05/03/2024

--- Assets/Scripts/CloudDrawer.cs
+++ Assets/Scripts/CloudDrawer.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using Task = System.Threading.Tasks.Task;
 
--- Assets/URP/RenderFeature/VolumetricAtmosphere.cs
+++ Assets/URP/RenderFeature/VolumetricAtmosphere.cs
@@ -1,95 +1,99 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-public class VolumetricAtmospherePass : ScriptableRenderPass
-{
 
-    private VolumetricAtmosphereSetting _settings;
-    private ProfilingSampler _profilingSampler;
-    private RTHandle _rtColor;
-    private Material _blitMat;
-
-    public VolumetricAtmospherePass(VolumetricAtmosphereSetting settings, string name, Material mat)
-    {
-        _settings = settings;
-        _profilingSampler = new ProfilingSampler(name);
-        _blitMat = mat;
-    }
-    public void SetTarget(RTHandle colorHandle)
-    {
-        _rtColor = colorHandle;
-    }
-    public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-    {
-        ConfigureTarget(_rtColor);
-    }
-
-    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-    {
-        CommandBuffer cmd = CommandBufferPool.Get();
-        using (new ProfilingScope(cmd, _profilingSampler))
-        {
-            context.ExecuteCommandBuffer(cmd);
-            cmd.Clear();
-            if (_blitMat != null)
-            {
-                _blitMat.SetTexture("_DepthTexture", renderingData.cameraData.renderer.cameraDepthTargetHandle);
-                _blitMat.SetFloat("_Camera_Near", renderingData.cameraData.camera.nearClipPlane);
-                _blitMat.SetFloat("_Camera_Far", renderingData.cameraData.camera.farClipPlane);
-                _blitMat.SetFloat("_AtmosphereHeight", _settings.AtmosphereHeight);
-                _blitMat.SetFloat("_EarthRadius", _settings.EarthRadius);
-                _blitMat.SetFloat("_AtmosphereDensityFalloff", _settings.AtmosphereDensityFalloff);
-                _blitMat.SetVector("_WaveLength", _settings.WaveLength);
-                _blitMat.SetFloat("_ScatterIntensity", _settings.ScatterIntensity);
-                _blitMat.SetFloat("_FinalColorMultiplier", _settings.FinalColorMultiplier);
-                _blitMat.SetFloat("_AnisotropicScattering", _settings.AnisotropicScatterig);
-                _blitMat.SetInt("_NumOpticalDepthSample", _settings.OpticalDepthSamples);
-                _blitMat.SetInt("_NumInScatteringSample", _settings.InScatteringSamples);
-
-                if (_rtColor != null)
-                {
-                    Blitter.BlitCameraTexture(cmd, _rtColor, _rtColor, _blitMat, 0);
-                }
-            }
-        }
-        context.ExecuteCommandBuffer(cmd);
-        cmd.Clear();
-        CommandBufferPool.Release(cmd);
-    }
-
-    public override void OnCameraCleanup(CommandBuffer cmd) { }
-    public void Dispose()
-    {
-        _rtColor?.Release();
-    }
-}
-
-[System.Serializable]
-public class VolumetricAtmosphereSetting
-{
-    public bool showInSceneView = true;
-    public RenderPassEvent _event = RenderPassEvent.AfterRenderingOpaques;
-
-
-    public float AtmosphereHeight = 10;
-    public float EarthRadius = 10000;
-    [Range(1, 10)]
-    public float AtmosphereDensityFalloff = 1;
-    [Range(0, 10)]
-    public float ScatterIntensity = 1;
-    [Range(0, 3)]
-    public float FinalColorMultiplier = 1;
-    [Range(1, 30)]
-    public int OpticalDepthSamples = 10;
-    [Range(1, 30)]
-    public int InScatteringSamples = 10;
-    [Range(0, 1)]
-    public float AnisotropicScatterig = 0;
-    public Vector3 WaveLength = new Vector3(700, 530, 440);
-}
 
 public class VolumetricAtmosphere : ScriptableRendererFeature
 {
+    public class VolumetricAtmospherePass : ScriptableRenderPass
+    {
+
+        private VolumetricAtmosphereSetting _settings;
+        private ProfilingSampler _profilingSampler;
+        private RTHandle _rtColor;
+        private Material _blitMat;
+
+        public VolumetricAtmospherePass(VolumetricAtmosphereSetting settings, string name, Material mat)
+        {
+            _settings = settings;
+            _profilingSampler = new ProfilingSampler(name);
+            _blitMat = mat;
+        }
+        public void SetTarget(RTHandle colorHandle)
+        {
+            _rtColor = colorHandle;
+        }
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            ConfigureTarget(_rtColor);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get();
+            using (new ProfilingScope(cmd, _profilingSampler))
+            {
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+                if (_blitMat != null)
+                {
+                    _blitMat.SetTexture("_DepthTexture", renderingData.cameraData.renderer.cameraDepthTargetHandle);
+                    _blitMat.SetFloat("_Camera_Near", renderingData.cameraData.camera.nearClipPlane);
+                    _blitMat.SetFloat("_Camera_Far", renderingData.cameraData.camera.farClipPlane);
+                    _blitMat.SetFloat("_AtmosphereHeight", _settings.AtmosphereHeight);
+                    _blitMat.SetFloat("_EarthRadius", _settings.EarthRadius);
+                    _blitMat.SetFloat("_AtmosphereDensityFalloff", _settings.AtmosphereDensityFalloff);
+                    _blitMat.SetVector("_WaveLength", _settings.WaveLength);
+                    _blitMat.SetFloat("_ScatterIntensity", _settings.ScatterIntensity);
+                    _blitMat.SetFloat("_FinalColorMultiplier", _settings.FinalColorMultiplier);
+                    _blitMat.SetFloat("_AnisotropicScattering", _settings.AnisotropicScatterig);
+                    _blitMat.SetInt("_NumOpticalDepthSample", _settings.OpticalDepthSamples);
+                    _blitMat.SetInt("_NumInScatteringSample", _settings.InScatteringSamples);
+
+                    if (_rtColor != null)
+                    {
+                        Blitter.BlitCameraTexture(cmd, _rtColor, _rtColor, _blitMat, 0);
+                    }
+                }
+            }
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void OnCameraCleanup(CommandBuffer cmd) { }
+        public void Dispose()
+        {
+            _rtColor?.Release();
+        }
+    }
+
+
+    [System.Serializable]
+    public class VolumetricAtmosphereSetting
+    {
+        public bool showInSceneView = true;
+        public RenderPassEvent _event = RenderPassEvent.AfterRenderingOpaques;
+
+
+        public float AtmosphereHeight = 10;
+        public float EarthRadius = 10000;
+        [Range(1, 10)]
+        public float AtmosphereDensityFalloff = 1;
+        [Range(0, 10)]
+        public float ScatterIntensity = 1;
+        [Range(0, 3)]
+        public float FinalColorMultiplier = 1;
+        [Range(1, 30)]
+        public int OpticalDepthSamples = 10;
+        [Range(1, 30)]
+        public int InScatteringSamples = 10;
+        [Range(0, 1)]
+        public float AnisotropicScatterig = 0;
+        public Vector3 WaveLength = new Vector3(700, 530, 440);
+    }
+
+
     public VolumetricAtmosphereSetting _settings = new VolumetricAtmosphereSetting();
     private VolumetricAtmospherePass _volumePass;
     private Material _blitMat;
@@ -120,6 +124,7 @@ public class VolumetricAtmosphere : ScriptableRendererFeature
     protected override void Dispose(bool disposing)
     {
         _volumePass.Dispose();
-        CoreUtils.Destroy(_blitMat);
+        if (!Application.isPlaying)
+            CoreUtils.Destroy(_blitMat);
     }
 }
